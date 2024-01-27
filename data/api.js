8ada67ded3bfe25b7650f7440ecdaa4308026e1a
Thiago Moabi <124590629+Tfreire455@users.noreply.github.com>
2024-01-27 19:56:20 -0300

    Update api.js

--- data/api.js
+++ data/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/Tfreire455/Aulas-Dio.me/main/5-%20portfolio-dio.me/data/profile.json'
+    const url = 'https://raw.githubusercontent.com/Tfreire455/Portfolio.dio/main/data/profile.json'
     const fetching = await fetch(url)
     return await fetching.json()
 }
